fd25ba23ce75d2f5697ef1b7b690966cb816aa59
Polina Petrenko <147924269+PolinaPetrenko@users.noreply.github.com>
2025-04-25 22:15:55 +0200

    hw-4 update

---
+++ migrations/20250425194012-add-owner-to-contacts.js
@@ -0,0 +1,28 @@
+// Міграція для додавання колонки 'owner' до таблиці 'contacts'
+
+import { DataTypes } from 'sequelize';
+
+export const up = async (queryInterface) => {
+  // Додаємо поле 'owner' до таблиці 'contacts' з можливістю бути NULL
+  await queryInterface.addColumn('contacts', 'owner', {
+    type: DataTypes.INTEGER,
+    allowNull: true, // Дозволяємо NULL значення тимчасово
+  });
+
+  // Оновлюємо всі контакти, щоб встановити значення 'owner'
+  await queryInterface.sequelize.query(`
+    UPDATE "contacts"
+    SET "owner" = 1;  -- Встановіть значення за замовчуванням, наприклад, 1
+  `);
+
+  // Тепер можна змінити allowNull на false, оскільки поле вже заповнено
+  await queryInterface.changeColumn('contacts', 'owner', {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  });
+};
+
+export const down = async (queryInterface) => {
+  // Видаляємо поле 'owner' у разі скасування міграції
+  await queryInterface.removeColumn('contacts', 'owner');
+};
